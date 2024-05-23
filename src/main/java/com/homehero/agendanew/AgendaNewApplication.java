7a70dbf70de3fc1b08f903b6aeccd2ac6f340aa1
darckronoz <crispipe14@gmail.com>
2024-05-23 07:03:36 -0500

    initial commit

---
+++ src/main/java/com/homehero/agendanew/AgendaNewApplication.java
@@ -0,0 +1,13 @@
+package com.homehero.agendanew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homehero/agendanew/AgendaNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homehero.agendanew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
